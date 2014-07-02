21ccd01657800874ccdfdfe4cb0b3f702e47d0ec
Shuhel Ahmed <shuhel@ymail.com>
2014-07-02 22:04:12 +0600

    added missing files from the previous commit

---
+++ source/Core/Api/Models/BlockUser.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Thinktecture.IdentityManager.Api.Models
+{
+    public class BlockUser
+    {
+        [Required]
+        public string Subject { get; set; }
+    }
+}
---
+++ source/Core/Api/Models/UnblockUser.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Thinktecture.IdentityManager.Api.Models
+{
+    public class UnblockUser
+    {
+        [Required]
+        public string Subject { get; set; }
+    }
+}
